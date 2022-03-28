6b7d91a3b602cd9cf74c21dc34882bc85275418c
junjieye <82857063+junjieye-nlp@users.noreply.github.com>
2022-03-28 10:34:49 +0800

    Update data-process.sh

--- data-process.sh
+++ data-process.sh
@@ -2,11 +2,11 @@
 
 python3 preprocess.py \
   --source-lang en \
-  --target-lang cs \
+  --target-lang de \
   --trainpref data-raw/train.bpe \
   --validpref data-raw/valid.bpe \
-  --testpref data-raw/test_2018_flickr.bpe \
+  --testpref data-raw/test_2016_flickr.bpe \
   --nwordssrc 17200 \
   --nwordstgt 9800 \
   --workers 12 \
-  --destdir /home/gb/yejunjie/mmt-coder/data-bin/en-cs/test2018 \
+  --destdir /home/gb/yejunjie/mmt-coder/data-bin/en-de/ \
